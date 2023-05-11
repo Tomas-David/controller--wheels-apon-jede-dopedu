7e2d9ee6ee47b8077b3360ca9fde7608c009ef37
Tomáš David <115162566+Tomas-David@users.noreply.github.com>
2023-05-11 11:29:52 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,10 @@ input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(0)
 })
 
-let tillL = 0
-let tillR = 0
+
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendNumber(200)
-    tillR = 0
-    tillL = 0
+    
 })
 
 function car_motor(lw: number = 0, rw: number = 0) {
@@ -25,22 +23,16 @@ function car_motor(lw: number = 0, rw: number = 0) {
 
 
 
-input.onButtonPressed(Button.A, function() {
-    tillL += 25
-})
 
-
-input.onButtonPressed(Button.B, function () {
-    tillR += 25
-})
 
 basic.forever(function () {
     radio.setGroup(20)
-    
+
+   
 
     radio.onReceivedNumber(function (receivedNumber) {
         if (receivedNumber > 0) {
-            car_motor(receivedNumber - tillL, receivedNumber - 143 - tillR)
+            car_motor(receivedNumber , receivedNumber - 143)
         } else {
             car_motor(0, 0)
         }

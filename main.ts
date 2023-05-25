11fbeaa0acfbba39f0435e28c9c51c224ef4bb40
Tomáš David <115162566+Tomas-David@users.noreply.github.com>
2023-05-25 09:58:05 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(253)
+})
 // logo jede dopředu
 // A měl by zatáčet do leva
 // B měl by zatáčet do prava
@@ -6,21 +8,12 @@
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(0)
 })
-
-input.onButtonPressed(Button.A, function () {
+input.onGesture(Gesture.TiltLeft, function () {
     radio.sendNumber(254)
 })
-
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(253)
-})
-
-
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendNumber(255)
-    
 })
-
 function car_motor(lw: number = 0, rw: number = 0) {
     const ul = Math.map(lw, -100, 100, -255, 255)
     const ur = Math.map(rw, -100, 100, -215, 215)
@@ -28,23 +21,18 @@ function car_motor(lw: number = 0, rw: number = 0) {
     PCAmotor.MotorRun(PCAmotor.Motors.M4, ul)
 
 }
-
 basic.forever(function () {
     radio.setGroup(20)
-
-   
-
     radio.onReceivedNumber(function (receivedNumber) {
         if (receivedNumber === 255) {
-            car_motor(receivedNumber, receivedNumber - 143 - 50)
-        }else if(receivedNumber === 254){
-            car_motor(receivedNumber - 75, receivedNumber - 143)
-        }else if (receivedNumber === 253) {
-            car_motor(receivedNumber, receivedNumber - 143 - 75)
+            car_motor(receivedNumber * -1, receivedNumber - 200)
+        } else if (receivedNumber === 253) {
+            car_motor(0, receivedNumber - 200)
+        } else if (receivedNumber === 254) {
+            car_motor(receivedNumber * -1, receivedNumber - 250)
         }
         else {
             car_motor(0, 0)
         }
     })
 })
-
